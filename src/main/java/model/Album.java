682bc878a24c4a9a0d2c87d2318d0b7f8dc561e7
Irina Tregub <irinatregub@gmail.com>
2020-05-24 20:52:50 +0300

    Adding Album to the project & AlbumDao interface

---
+++ src/main/java/model/Album.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Album {
+    private int id;
+    private String title;
+    private int artistId;
+
+    public Album(String title, int artistId) {
+        this.title = title;
+        this.artistId = artistId;
+    }
+
+    public Album(int id, String title, int artistId) {
+        this.id = id;
+        this.title = title;
+        this.artistId = artistId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getArtistId(){return this.artistId;}
+}
